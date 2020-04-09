4d90d4345e571ce3052bf49fe8dd6a9c15adbc1a
Darkhonbek Mamataliev <11254619+darhonbek@users.noreply.github.com>
2020-04-09 00:32:21 -0400

    Update CTCI-1.3.swift
    Add input and expected output data. Make code shorter.

--- CTCI-1.3.swift
+++ CTCI-1.3.swift
@@ -13,32 +13,39 @@ extension Int {
 
 func isPalindromePermutation(string s: String) -> Bool {
     let counter = countCharacterFrequency(in: s)
+    var oddsCount = counter.filter { $0%2 == 1 }.count
     
-    var oddsCount = 0
-    for count in counter where count%2 == 1 {
-        guard oddsCount == 0 else { return false }
-        oddsCount += 1
-    }
-    
-    return true
+    return oddsCount < 2
 }
 
 func countCharacterFrequency(in s: String) -> [Int] {
     var counter = Array(repeating: 0, count: 26)
     
     for c in s {
-        if let ascii = Int(c.asciiValue) {
-            if ascii >= 65 && ascii <= 90 {         // A-Z
-                counter[ascii + 32 - 97] += 1
-            } else if ascii >= 97 && ascii <= 122 { // a-z
-                counter[ascii - 97] += 1
-            }
+        if var ascii = Int(c.asciiValue) {
+            ascii -= 65 // Subtract ASCII value of 'A'.
+            if ascii > 31 && ascii < 58 { ascii -= 32 } // Make a-z -> A-Z.
+            if ascii > -1 && ascii < 26 { counter[ascii] += 1 }
         }
     }
     
     return counter
 }
 
-var string = "Polop palap"
-let result = isPalindromePermutation(string: string)
+var input = [
+    "Polops fpalap",
+    "Polop palap",
+    "Jackie Chan",
+    "Tact Coa"
+]
+
+let output = input.map { isPalindromePermutation(string: $0) }
+
+var expectedOutput = [
+    false,
+    true,
+    false,
+    true
+]
+var result = output == expectedOutput ? "Accepted" : "Failed"
 print(result)
