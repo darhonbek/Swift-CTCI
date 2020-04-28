3fa53d6019eb8587fd2263e50244fed5eb7e78d7
Darkhonbek Mamataliev <11254619+darhonbek@users.noreply.github.com>
2020-04-28 00:42:52 -0400

    Create F1.swift

---
+++ F1.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+func nearestCommonView(a: UIView, b: UIView) -> UIView? {
+    var containingView: UIView?
+    let aSuperviews = a.getSuperviews()
+
+    b.forEachSuperview {view, shouldBreak in
+        if aSuperviews.contains(view) {
+            containingView = view
+            shouldBreak = true
+        }
+    }
+
+    return containingView
+}
+
+extension UIView {
+    func getSuperviews() -> Set<UIView> {
+        var superviewSet = Set<UIView>()
+        forEachSuperview { view, _ in
+            superviewSet.insert(view)
+        }
+
+        return superviewSet
+    }
+
+    func forEachSuperview(shouldIncludeSelf: Bool = true, closure: @escaping (_ view: UIView, _ shouldBreak: inout Bool) -> ()) {
+        var tempView: UIView? = shouldIncludeSelf ? self : self.superview
+        var shouldBreak = false
+
+        while let view = tempView {
+            closure(view, &shouldBreak)
+            if shouldBreak { break }
+            tempView = tempView?.superview
+        }
+    }
+}
+
+// TEST:
+var views = [
+    UIView(),
+    UIView(),
+    UIView(),
+    UIView(),
+    UIView()
+]
+
+views[2].addSubview(views[0])
+views[2].addSubview(views[1])
+views[4].addSubview(views[2])
+views[4].addSubview(views[3])
+
+var output = nearestCommonView(a: views[0], b: views[1])
+var expectedOutput = views[2]
+
+print(output == expectedOutput ? "Accepted" : "Failed")
