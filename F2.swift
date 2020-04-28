8292f64ee193468b3ca530b39efc74dcd5449a7a
Darkhonbek Mamataliev <11254619+darhonbek@users.noreply.github.com>
2020-04-28 01:34:23 -0400

    Create F2.swift

---
+++ F2.swift
@@ -0,0 +1,28 @@
+func pathsToCell(_ r: Int, _ c: Int, _ path: String = "") -> [String] {
+    guard r > 0, c > 0 else { return [] }
+    
+    var paths: [String] = []
+    
+    if r == 1 && c == 1 { // origin
+        paths.append(path)
+    } else {
+        paths.append(contentsOf: pathsToCell(r-1, c, path + "D"))
+        paths.append(contentsOf: pathsToCell(r, c-1, path + "R"))
+    }
+    
+    return paths
+}
+
+// TEST:
+
+let input = [1, 2, 3]
+let expectedOutput = [
+    [""],
+    ["DR", "RD"],
+    ["DDRR", "DRDR", "DRRD", "RDDR", "RDRD", "RRDD"]
+]
+
+let output = input.map { pathsToCell($0, $0) }
+let result = output == expectedOutput ? "Accepted" : "Failed"
+
+print(result)
