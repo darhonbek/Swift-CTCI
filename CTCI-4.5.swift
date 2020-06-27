65fd5148335915b85c8a2a278b108965edef5a07
Darkhonbek Mamataliev <11254619+darhonbek@users.noreply.github.com>
2020-06-27 00:27:34 -0400

    Update CTCI-4.5.swift
    Fix BST validation.

--- CTCI-4.5.swift
+++ CTCI-4.5.swift
@@ -9,57 +9,72 @@ class TreeNode {
 }
 
 func isBST(_ root: TreeNode?) -> Bool {
-    guard let root = root else { return false }
-    
-    var queue = [root]
-    var lvlQueue: [TreeNode]
-    
-    while !queue.isEmpty {
-        lvlQueue = []
-        
-        for node in queue {
-            if let leftNode = node.left {
-                if leftNode.val <= node.val {
-                    lvlQueue.append(leftNode)
-                } else {
-                    return false
-                }
-            }
-            
-            if let rightNode = node.right {
-                if rightNode.val >= node.val {
-                    lvlQueue.append(rightNode)
-                } else {
-                    return false
-                }
-            }
-        }
-        
-        queue = lvlQueue
-    }
-    
-    return true
+    guard let root = root else { return true }
+    let (flag, _, _) = dfs(root)
+    return flag
 }
 
-var treeRoot = TreeNode(7)
+private func dfs(_ node: TreeNode) -> (Bool, Int, Int) {
+    guard node.left != nil || node.right != nil else { // base case, exit
+        return (true, node.val, node.val)
+    }
 
-treeRoot.left = TreeNode(5)
-treeRoot.right = TreeNode(9)
+    var left: (flag: Bool, max: Int, min: Int)?
+    var right: (flag: Bool, max: Int, min: Int)?
 
-treeRoot.left?.left = TreeNode(4)
+    if let leftNode = node.left { left = dfs(leftNode) }
+    if let rightNode = node.right { right = dfs(rightNode) }
+
+    guard left?.flag != false && right?.flag != false else { // child node is not BST, exit
+        return (false, node.val, node.val)
+    }
+
+    var flag: Bool = true
+    flag = flag && node.val > (left?.max ?? node.val - 1)
+    flag = flag && node.val < (right?.min ?? node.val + 1)
+
+    return (
+        flag,
+        maxOptional(maxOptional(node.val, left?.max), right?.max),
+        minOptional(minOptional(node.val, left?.min), right?.min)
+    )
+}
+
+private func minOptional(_ lhs: Int, _ rhs: Int?) -> Int {
+    guard let rhs = rhs else { return lhs }
+    return min(lhs, rhs)
+}
+
+private func maxOptional(_ lhs: Int, _ rhs: Int?) -> Int {
+    guard let rhs = rhs else { return lhs }
+    return max(lhs, rhs)
+}
+
+var treeRoot = TreeNode(5)
+
+treeRoot.left = TreeNode(2)
+treeRoot.right = TreeNode(8)
+
+treeRoot.left?.left = TreeNode(1)
 treeRoot.left?.right = TreeNode(6)
-treeRoot.right?.left = TreeNode(8)
+treeRoot.right?.left = TreeNode(0)
 treeRoot.right?.right = TreeNode(10)
 
-var treeRoot2 = TreeNode(1)
-treeRoot2.left = TreeNode(2)
+var treeRoot2 = TreeNode(2)
+treeRoot2.left = TreeNode(1)
 treeRoot2.right = TreeNode(3)
 
-let input = [treeRoot, treeRoot2]
+var treeRoot3 = TreeNode(0)
+treeRoot3.left = TreeNode(-1)
+
+var treeRoot4 = TreeNode(1)
+treeRoot4.left = TreeNode(1)
+
+let input = [treeRoot, treeRoot2, treeRoot3, treeRoot4]
 
 let output = input.map { isBST($0) }
 
-let expectedOutput = [true, false]
+let expectedOutput = [false, true, true, false]
 
 let result = output == expectedOutput ? "Accepted" : "Failed"
 print(result)
