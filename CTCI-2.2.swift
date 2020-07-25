b3f09241ea99cafcf98261d6009f4c6453cc4f05
Darkhonbek Mamataliev <11254619+darhonbek@users.noreply.github.com>
2020-07-25 18:23:06 -0400

    Update CTCI-2.2.swift

--- CTCI-2.2.swift
+++ CTCI-2.2.swift
@@ -1,43 +1,47 @@
-class Node {
-    var value: String?
-    var next: Node?
-    
-    init(_ value: String, next: Node? = nil) {
-        self.value = value
-        self.next = next
+// Return kth to last node in a singly linked list.
+//
+// Leetcode problem asks to delete kth to last node:
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+
+/**
+Solution:
+1. Create pointers first and second and move them (k + 1) nodes apart.
+2. Iterate both, until second reached the end. At that point first will be (k - 1)th from end node.
+3. Set first.next = first.next.next, this wat k-th node is removed.
+*/
+class Solution {
+    func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+        var first = head
+        var second = head
+        
+        for i in 0 ..< n {
+            second = second?.next
+        }
+
+        if second == nil {
+            return head?.next
+        }
+        
+        var prev: ListNode?
+        
+        while second != nil {
+            prev = first
+            first = first?.next
+            second = second?.next
+        }
+        
+        prev?.next = first?.next
+        
+        return head
     }
 }
-
-func removeNode(_ node: Node?) {
-    guard let next = node?.next else {
-        node?.value = nil
-        return
-    }
-
-    node?.value = next.value
-    node?.next = next.next
-}
-
-func getNodeValues(startingFrom node: Node) -> [String?] {
-    var values: [String?] = []
-    var runner: Node? = node
-    
-    while runner != nil {
-        values.append(runner?.value)
-        runner = runner?.next
-    }
-    
-    return values
-}
-
-var n3 = Node("3")
-var n2 = Node("2", next: n3)
-var n1 = Node("1", next: n2)
-
-let expectedOutput = ["1", "2"]
-let output: [String] = {
-    removeNode(n3)
-    return getNodeValues(startingFrom: n1).compactMap { $0 }
-}()
-let result = output == expectedOutput ? "Accepted" : "Failed"
-print(result)
