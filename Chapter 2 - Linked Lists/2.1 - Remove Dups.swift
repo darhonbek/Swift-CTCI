e45b4a7142ddfc8e4f76c2f14a6ebe7c1b5e7202
Darkhonbek Mamataliev <darkhonbek@gmail.com>
2020-09-21 02:00:19 -0400

    Refactor problem 2.1 to use LinkedList instead of Screens.

--- Chapter 2 - Linked Lists/2.1 - Remove Dups.swift
+++ Chapter 2 - Linked Lists/2.1 - Remove Dups.swift
@@ -1,79 +1,58 @@
-class Screen {
-    let name: String
-    var next: Screen?
-    
-    init(_ name: String, next: Screen? = nil) {
-        self.name = name
-        self.next = next
-    }
-}
+// Write a code to remove a duplicates from an unsorted linked list.
+//
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
 
-// O(n) speed, O(n) memory
-func removeDuplicates(from screen: Screen) {
-    var visitedScreens: Set<String> = [screen.name]
-    var previousScreen: Screen?
-    var nextScreen = screen.next
-    
-    while nextScreen != nil {
-        guard let screenName = nextScreen?.name else { continue }
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+ 
+class Solution {
+    // O(n) speed, O(n) memory
+    func deleteDuplicates(_ head: ListNode?) -> ListNode? {
+        var visitedNodes: Set<Int?> = [head?.val]
+        var prev = head
+        var node = head?.next
         
-        if visitedScreens.contains(screenName) {
-            previousScreen?.next = nextScreen?.next
-        } else {
-            visitedScreens.insert(screenName)
-            previousScreen = nextScreen
-        }
-        
-        nextScreen = nextScreen?.next
-    }
-}
-
-// O(n^2) speed, O(1) memory
-func removeDuplicatesO1Memory(from screen: Screen) {
-    var currentScreen: Screen? = screen
-    
-    while currentScreen != nil {
-        var runner = currentScreen
-        
-        while runner?.next != nil {
-            if runner?.next?.name == currentScreen?.name {
-                runner?.next = runner?.next?.next
+        while node != nil {
+            if visitedNodes.contains(node?.val) {
+                prev?.next = node?.next
             } else {
-                runner = runner?.next
+                visitedNodes.insert(node?.val)
+                prev = node
             }
+            
+            node = node?.next
         }
         
-        currentScreen = currentScreen?.next
+        return head
+    }
+
+    // O(n^2) speed, O(1) memory
+    func deleteDuplicates(_ head: ListNode?) -> ListNode? {
+        var current = head
+        var runner: ListNode?
+        
+        while current != nil {
+            runner = current
+            
+            while runner?.next != nil {
+                if runner?.next?.val == current?.val {
+                    runner?.next = runner?.next?.next
+                } else {
+                    runner = runner?.next
+                }
+            }
+            
+            current = current?.next
+        }
+        
+        return head
     }
 }
-
-func getScreenOrder(startingFrom screen: Screen) -> [String] {
-    var screens: [String] = []
-    screens.append(screen.name)
-    var nextScreen = screen.next
-    
-    while nextScreen != nil {
-        guard let screenName = nextScreen?.name else { continue }
-        screens.append(screenName)
-        nextScreen = nextScreen?.next
-    }
-    
-    return screens
-}
-
-let s7 = Screen("4")
-let s6 = Screen("4", next: s7)
-let s5 = Screen("1", next: s6)
-let s4 = Screen("3", next: s5)
-let s3 = Screen("2", next: s4)
-let s2 = Screen("2", next: s3)
-let s1 = Screen("1", next: s2)
-
-let input = s1
-let expectedOutput = ["1", "2", "3", "4"]
-let output: [String] = {
-    removeDuplicatesO1Memory(from: s1)
-    return getScreenOrder(startingFrom: s1)
-}()
-let result = output == expectedOutput ? "Accepted" : "Failed"
-print(result)
