9b60ab3ff62a39ecef7168add9cfb4eac803be32
Darkhonbek Mamataliev <darkhonbek@gmail.com>
2020-09-21 22:27:40 -0400

    Add problem 8.9.

---
+++ Chapter 8 - Dynamic Programming/8.9 - Parens.swift
@@ -0,0 +1,50 @@
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+//
+// https://leetcode.com/problems/generate-parentheses/
+
+/**
+Solution:
+1. Start recursion and pass:
+    - # of open left
+    - # of closed left
+    - path so far
+    - empty mutable array to keep track of paths.
+2. Base case when open == 0 and closed == 0, append path to array.
+3. Non base case: 
+    - if open > 0, start recursion with path + "(" and open - 1.
+    - if closed < open, start recursion with path + ")" and closed - 1.
+4. Return array.
+
+Complexity:
+Time = Space = Catalan numbers: C(n) = binomial(2n,n)/(n+1) = (2n)!/(n!(n+1)!)
+Derived from http://oeis.org.
+
+Edge cases:
+- n < 1
+- order of combinations?
+*/
+
+class Solution {
+    func generateParenthesis(_ n: Int) -> [String] {
+        guard n > 0 else { return [] }
+        
+        var combinations: [String] = []
+        generateCombinations(open: n - 1, closed: n, path: "(", array: &combinations)
+        return combinations
+    }
+    
+    private func generateCombinations(open: Int, closed: Int, path: String, array: inout [String]) {
+        guard open > 0 || closed > 0 else {
+            array.append(path)
+            return
+        }
+        
+        if open > 0 {
+            generateCombinations(open: open - 1, closed: closed, path: path + "(", array: &array)
+        }
+        
+        if closed > open {
+            generateCombinations(open: open, closed: closed - 1, path: path + ")", array: &array)
+        }
+    }
+}
